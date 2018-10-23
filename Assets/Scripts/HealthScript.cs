2a32dff4a25e3c90c7a953e40fc95c0a2d072415
Majersky, Timotej (tm576d) <tm576d@intl.att.com>
2018-10-23 18:59:25 +0200

    INIT adding silly assets

---
+++ Assets/Scripts/HealthScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthScript : MonoBehaviour {
+
+    public int hp = 1;
+    public bool isEnemy = true;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Damage(int damageCount)
+    {
+        hp -= damageCount;
+
+        if (hp <= 0)
+        {
+
+            // Dead!
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        // Check if the colided item is a shot
+        ShotScript shot = otherCollider.gameObject.GetComponent<ShotScript>();
+        if (shot != null)
+        {
+            // friendly fire off
+            if (shot.isEnemyShot != isEnemy)
+            {
+                Damage(shot.damage);
+
+                // Destroy the shot
+                Destroy(shot.gameObject); 
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ShotScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotScript : MonoBehaviour
+{
+    // 0 - Designer variables
+
+    /// <summary>
+    /// Damage inflicted
+    /// </summary>
+    public int shotLife = 20;
+    public int damage = 1;
+
+    /// <summary>
+    /// Projectile damage player or enemies?
+    /// </summary>
+    public bool isEnemyShot = false;
+
+    void Start()
+    {
+        // 1 - Limited time to live to avoid any leak
+        Destroy(gameObject, shotLife); // 20sec
+    }
+}
